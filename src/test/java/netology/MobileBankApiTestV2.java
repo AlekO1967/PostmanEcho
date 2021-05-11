9764ae129120ead43c0bda6bcb8ef2232a4f9418
Aleksandr Osipov <eltisprim@mail.ru>
2021-05-11 16:54:39 -0400

    Added tests

---
+++ src/test/java/netology/MobileBankApiTestV2.java
@@ -0,0 +1,23 @@
+package netology;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class MobileBankApiTestV2 {
+    @Test
+    void shouldReturnDemoAccounts() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .contentType(ContentType.JSON)
+        ;
+    }
+}
