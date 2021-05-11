c71f1ccc11af230705bd4c69bd94732917ea9de5
Aleksandr Osipov <eltisprim@mail.ru>
2021-05-11 17:16:17 -0400

    Fixed schema

--- src/test/java/ru/netology/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/MobileBankApiTestV1.java
@@ -1,4 +1,4 @@
-package netology;
+package ru.netology;
 
 import org.junit.jupiter.api.Test;
 
--- src/test/java/ru/netology/MobileBankApiTestV2.java
+++ src/test/java/ru/netology/MobileBankApiTestV2.java
@@ -1,4 +1,4 @@
-package netology;
+package ru.netology;
 
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
